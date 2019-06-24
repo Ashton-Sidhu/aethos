eda0c3e950893d96b43452ff357396d1fd6a09e9
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2019-06-24 19:34:02 -0400

    added function that removes columns that have > than a threshold % of missing values

--- cleaning/clean.py
+++ cleaning/clean.py
@@ -6,7 +6,18 @@ from utils import *
 class Clean(CleanUtil):
 
     def __init__(self, data):
-        CleanBase.__init__(self, data)        
+        CleanBase.__init__(self, data)
+
+    def RemoveColumns(self, threshold):
+        """Removed columns from the dataframe that have more than the threshold value of missing columns.
+        Example: Remove columns where > 50% of the data is missing
+        
+        Arguments:
+            threshold {[float]} -- Value between 0 and 1 that describes what percentage of a column can be missing values.
+        """
+        
+        criteria_meeting_columns = self.df.columns[self.df.isnull().mean() < threshold]
+        self.df = self.df[criteria_meeting_columns]      
 
     def CleanMissingData(self, custom_cols={}, analysis="time_agnostic"):
         return
