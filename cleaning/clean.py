70c6e6e6e5c65722af861369d37bac68e2121acf
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2019-06-25 00:10:18 -0400

    added removing rows if missing values are greater than a certain percentage

--- cleaning/clean.py
+++ cleaning/clean.py
@@ -10,7 +10,7 @@ class Clean(CleanUtil, CleanNumeric):
         CleanBase.__init__(self, data)
 
     def RemoveColumns(self, threshold):
-        """Removed columns from the dataframe that have more than the threshold value of missing columns.
+        """Remove columns from the dataframe that have more than the threshold value of missing columns.
         Example: Remove columns where > 50% of the data is missing
         
         Arguments:
@@ -18,13 +18,17 @@ class Clean(CleanUtil, CleanNumeric):
         """
         
         criteria_meeting_columns = self.df.columns[self.df.isnull().mean() < threshold]
-        self.df = self.df[criteria_meeting_columns]      
+        self.df = self.df[criteria_meeting_columns]
 
-    def CleanMissingData(self, custom_cols={}, analysis="time_agnostic"):
-        return
+    def RemoveRows(self, threshold):
+        """Remove rows from the dataframe that have more than the threshold value of missing rows.
+        Example: Remove rows where > 50% of the data is missing
+        
+        Arguments:
+            threshold {[float]} -- Value between 0 and 1 that describes what percentage of a row can be missing values.
+        """
 
-    def CleanData(self):
-        return
+        self.df.dropna(thresh=int(self.df.shape[1] * threshold), inplace=True, axis=0)    
 
     def GenerateCode(self):
         return
