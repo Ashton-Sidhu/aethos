94fa7568a3c15c9e41443402af4b7d73ee6f50fe
big_sidhu <big_sidhu@secdev.io>
2019-06-28 12:10:22 -0400

    added param to use full dataset

--- preprocessing/numeric.py
+++ preprocessing/numeric.py
@@ -3,5 +3,5 @@ from base import PreprocessBase
 
 class PreprocessNumeric(PreprocessBase):
 
-    def PreprocessNormalize(self, custom_cols=[], override=False):
+    def PreprocessNormalize(self, use_full_dataset=False, custom_cols=[], override=False):
         return
