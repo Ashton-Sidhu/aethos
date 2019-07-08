12cc8fe313b6e4ee7465fb3f1e5d31dcfaaff7c6
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2019-07-08 01:51:26 -0400

    updated setup file

--- setup.py
+++ setup.py
@@ -2,5 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='py_automl',
+    url='https://github.com/Ashton-Sidhu/py-automl'
     packages=find_packages()
+    author='Ashton Sidhu',
+    author_email='ashton.sidhu1994@gmail.com',
+    install_requires=['numpy', 'pandas', 'scikit-learn'],
+    version='0.1',
+    license='MIT',
+    description='A library of data science and machine learning techniques to help automate workflow.',
+    long_description=open('README.md').read(),
 )
