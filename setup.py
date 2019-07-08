db78099cda41fbdc23adbe339467f3dfcddf2df5
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2019-07-08 01:57:23 -0400

    fixed syntax error

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name='py_automl',
     url='https://github.com/Ashton-Sidhu/py-automl'
-    packages=find_packages()
+    packages=find_packages(),
     author='Ashton Sidhu',
     author_email='ashton.sidhu1994@gmail.com',
     install_requires=['numpy', 'pandas', 'scikit-learn'],
     version='0.1',
     license='MIT',
     description='A library of data science and machine learning techniques to help automate workflow.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md').read()
 )
