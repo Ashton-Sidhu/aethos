a11fcf2871f505c7c002c23b079e4b2c226bf609
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2019-07-13 02:13:30 -0400

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Ashton Sidhu',
     author_email='ashton.sidhu1994@gmail.com',
     install_requires=['numpy', 'pandas', 'scikit-learn'],
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='A library of data science and machine learning techniques to help automate workflow.',
     long_description=long_description,
