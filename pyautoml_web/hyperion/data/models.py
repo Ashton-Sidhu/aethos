fe570cb721ac9568220b087a81517101ba193aad
ashton-sidhu <ashton.sidhu1994@gmail.com>
2019-08-09 21:37:11 -0700

    created data model;

--- pyautoml_web/hyperion/data/models.py
+++ pyautoml_web/hyperion/data/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Source(models.Model):
+
+    data_name = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    path = models.CharField(max_length=200, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.data_name
