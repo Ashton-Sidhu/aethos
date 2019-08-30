8b47d854860726efaa46ce47d5e59eef46812926
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2019-08-30 00:57:10 -0700

    updated release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Ashton Sidhu',
     author_email='ashton.sidhu1994@gmail.com',
     install_requires=['numpy', 'pandas', 'scikit-learn', 'textblob', 'pandas_summary', 'pandas-bokeh', 'ptitprince', 'nltk', 'ipython'],
-    version='0.3.0.1',
+    version='0.3.1',
     license='GPL-3.0',
     description='A library of data science and machine learning techniques to help automate your workflow.',
     long_description=long_description,
