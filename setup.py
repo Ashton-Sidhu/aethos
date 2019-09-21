3754ea955cef0101fb2d1d0c4074922a2f051cdb
ashton-sidhu <ashton.sidhu1994@gmail.com>
2019-09-21 06:28:41 -0400

    updated  setup and requirements

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     author_email='ashton.sidhu1994@gmail.com',
     install_requires=['numpy', 'pandas>=0.25', 'scikit-learn', 'textblob', 'matplotlib<3.2.0',
                       'pandas_summary', 'pandas-bokeh', 'ptitprince', 'nltk', 'ipython', 'gensim',
-                      'pandas-profiling', 'cookiecutter'],
+                      'pandas-profiling', 'cookiecutter', 'shap'],
     version=VERSION,
     license='GPL-3.0',
     description='A library of data science and machine learning techniques to help automate your workflow.',
