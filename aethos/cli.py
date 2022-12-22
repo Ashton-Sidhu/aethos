392d44fe784765db5cd5cae9521bd24bf453f469
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2022-12-21 19:41:46 -0500

    Update packages in CI

--- aethos/cli.py
+++ aethos/cli.py
@@ -9,24 +9,6 @@ from aethos.config.user_config import EXP_DIR
 def main():
     pass
 
-
-@main.command()
-def create():
-    """
-    Creates a Data Science folder structure and files.
-    """
-    os.system("cookiecutter https://github.com/drivendata/cookiecutter-data-science")
-
-
-@main.command()
-def enable_extensions():
-    """
-    Enables jupyter extensions such as qgrid.
-    """
-    os.system("jupyter nbextension enable --py --sys-prefix widgetsnbextension")
-    os.system("jupyter nbextension enable --py --sys-prefix qgrid")
-
-
 @main.command()
 def install_corpora():
     """
